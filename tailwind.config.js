594a55a9d955a5592ff437985428d493310f13b9
Mykola Darnichenko <programmboy@gmail.com>
2024-07-10 12:01:21 +0300

    add some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
     screens: {
       'sm': '549px',
       // 'sm': '640px',
